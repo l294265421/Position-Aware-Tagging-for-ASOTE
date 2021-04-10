4a198f78f01c3b2238a2425f06029bbd75e75ee1
yuncongli <yuncongli@tencent.com>
2021-04-10 19:54:43 +0800

    set the touch thread number = 16

--- triplet/hypergraph/NetworkConfig.py
+++ triplet/hypergraph/NetworkConfig.py
@@ -8,7 +8,7 @@ class LossType(Enum):
 
 class NetworkConfig:
     DEFAULT_CAPACITY_NETWORK = np.asarray([1000,1000, 1000, 1000, 1000], dtype=int)
-    NUM_THREADS = 1
+    NUM_THREADS = 16
     GPU_ID = -1
     DEVICE = torch.device("cpu")  #device = torch.device("cuda:" + args.gpuid)
     NEURAL_LEARNING_RATE = 0.015
